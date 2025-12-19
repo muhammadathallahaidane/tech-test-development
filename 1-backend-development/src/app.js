a3218e323576b7cdf23e72081b4618125fc3b1f4
muhammadathallahaidane <talaaidane@gmail.com>
2025-12-19 17:26:27 +0700

    feat: handle submit data to database

--- 1-backend-development/src/app.js
+++ 1-backend-development/src/app.js
@@ -1,14 +1,13 @@
-const express = require("express")
-const app = express()
-const routes = require("./routes")
-const port = 3000
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", routes)
+app.use("/api", routes);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-    
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- 1-backend-development/src/routes/index.js
+++ 1-backend-development/src/routes/index.js
@@ -1,8 +1,7 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/controller")
 
-router.get('/', (req, res) => {
-  res.send('Test express routing')
-})
+router.post("/employee", controller.submitData);
 
-module.exports = router
+module.exports = router;
