e9d7939f433bb660e3f1a8018b226089abe7e56e
muhammadathallahaidane <talaaidane@gmail.com>
2025-12-19 18:17:39 +0700

    feat: create simple frontend for user

--- 1-backend-development/src/app.js
+++ 1-backend-development/src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const port = 3000;
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use("/api", routes);
 
