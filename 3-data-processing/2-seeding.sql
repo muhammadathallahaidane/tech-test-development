c9122dfaaaea49940360bd82ba537ad4f8db6c48
muhammadathallahaidane <talaaidane@gmail.com>
2025-12-20 01:27:28 +0700

    feat: task 3 queries and creating readme

---
+++ 3-data-processing/2-seeding.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (name, position, join_date, release_date, year_of_experience, salary) VALUES 
+('Jacky', 'Solution Architect', '2018-07-25', '2022-07-25', 8, 150),
+('John', 'Assistan Manager', '2016-02-02', '2021-02-02', 12, 155),
+('Alano', 'Manager', '2010-11-09', NULL, 14, 175),
+('Aaron', 'Engineer', '2021-08-16', '2022-08-16', 1, 80),
+('Allen', 'Engineer', '2024-06-06', NULL, 4, 75),
+('Peter', 'Team Leader', '2020-01-09', NULL, 3, 85);
